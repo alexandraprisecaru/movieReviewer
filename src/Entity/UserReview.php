f9efdd41595ed6a2df7f3fc51270539a845bd497
Blamoursie <prisecaru.ale@gmail.com>
2019-02-12 00:01:36 +0200

    Log in sort of working. role based access still in work.

--- src/Entity/UserReview.php
+++ src/Entity/UserReview.php
@@ -5,11 +5,12 @@ namespace App\Entity;
 use Doctrine\Common\Collections\ArrayCollection;
 use Doctrine\Common\Collections\Collection;
 use Doctrine\ORM\Mapping as ORM;
+use Symfony\Component\Security\Core\User\UserInterface;
 
 /**
  * @ORM\Entity(repositoryClass="App\Repository\UserReviewRepository")
  */
-class UserReview
+class UserReview implements UserInterface, \Serializable
 {
     /**
      * @ORM\Id()
@@ -93,7 +94,7 @@ class UserReview
 
         return $this;
     }
-    
+
     public function getLastName(): ?string
     {
         return $this->lastName;
@@ -149,11 +150,99 @@ class UserReview
         return $this;
     }
 
-    public function __toString() {
+    public function __toString()
+    {
         try {
-            return (string) $this->firstName;
+            return (string)$this->firstName;
         } catch (Exception $exception) {
             return '';
         }
+    }
+
+    private $roles = [];
+
+    /**
+     * Returns the roles granted to the user.
+     *
+     *     public function getRoles()
+     *     {
+     *         return array('ROLE_USER');
+     *     }
+     *
+     * Alternatively, the roles might be stored on a ``roles`` property,
+     * and populated in any number of different ways when the user object
+     * is created.
+     *
+     * @return (Role|string)[] The user roles
+     */
+    public function getRoles()
+    {
+        return [];
+//        $roles = ['ROLE_ADMIN', 'ROLE_USER'];
+////        $roles[] = 'ROLE_USER';
+//
+//        return $roles;
+    }
+
+    /**
+     * Returns the salt that was originally used to encode the password.
+     *
+     * This can return null if the password was not encoded using a salt.
+     *
+     * @return string|null The salt
+     */
+    public function getSalt()
+    {
+        // TODO: Implement getSalt() method.
+    }
+
+    /**
+     * Removes sensitive data from the user.
+     *
+     * This is important if, at any given point, sensitive information like
+     * the plain-text password is stored on this object.
+     */
+    public function eraseCredentials()
+    {
+        // TODO: Implement eraseCredentials() method.
+    }
+
+    /**
+     * String representation of object
+     * @link https://php.net/manual/en/serializable.serialize.php
+     * @return string the string representation of the object or null
+     * @since 5.1.0
+     */
+    public function serialize()
+    {
+        return serialize([
+            $this->id,
+            $this->username,
+            $this->firstName,
+            $this->lastName,
+            $this->password,
+            $this->role
+        ]);
+    }
+
+    /**
+     * Constructs the object
+     * @link https://php.net/manual/en/serializable.unserialize.php
+     * @param string $serialized <p>
+     * The string representation of the object.
+     * </p>
+     * @return void
+     * @since 5.1.0
+     */
+    public function unserialize($string)
+    {
+        list(
+            $this->id,
+            $this->username,
+            $this->firstName,
+            $this->lastName,
+            $this->password,
+            $this->role
+            ) = $this->unserialize($string, ['allowed_classes' => false]);
     }
 }
